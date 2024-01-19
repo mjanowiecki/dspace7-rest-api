311c1ec1d503a2f14f2af2f7c76002201c627cc1
Noah Geraci <noah.geraci@jhu.edu>
2024-01-19 12:48:36 -0800

    first pass at getting getItemsMetadataByItemID working

--- get/getItemsMetadataByItemID.py
+++ get/getItemsMetadataByItemID.py
@@ -1,31 +1,63 @@
-import requests
+""" This script gets item metadata from the DSpace 7 API and outputs it in CSV format.
+
+    As input, it takes a CSV file that lists one or more item UUIDs
+    in a column labeled "item_id"
+
+    TODO: add support for handle as item ID
+"""
+
 import argparse
+import requests
 import pandas as pd
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--file')
-args = parser.parse_args()
 
-if args.file:
-    filename = args.file
-else:
-    filename = input('Enter filename (including \'.csv\'): ')
+def main():
+    # parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file")
+    args = parser.parse_args()
+    if args.file:
+        filename = args.file
+    else:
+        # prompt user to enter filename if not provided as arg
+        filename = input("Enter filename (including '.csv'): ")
+
+    # set base url and timeout
+    base_url = "https://j10p-stage.library.jhu.edu"
+    timeout = 10
+
+    # iterate over input csv
+    df = pd.read_csv(filename)
+    all_items = []
+    for index, row in df.iterrows():
+        item_id = row["item_id"]
+        print(index, item_id)
+        # get item data
+        item_endpoint = f"/server/api/core/items/{item_id}"
+        r = requests.get(f"{base_url}/{item_endpoint}", timeout=timeout)
+        if r.status_code == 200:
+            item_dict = json_to_flat_dict(r.json())
+            all_items.append(item_dict)
+        # TODO: error handling for timeouts and non-200 statuses
+
+    # output csv
+    all_items = pd.DataFrame.from_dict(all_items)
+    all_items.to_csv("item_metadata.csv", index=False)
 
 
-header = {'content-type': 'application/json'}
-baseURL = 'https://j10p-clone.library.jhu.edu'
+def json_to_flat_dict(item):
+    """
+    take JSON item with nested/multi-value fields returned from DSpace 7 API
 
-df = pd.read_csv(filename)
-
-all_items = []
-for count, row in df.iterrows():
-    row = row
-    item_id = row['item_id']
-    print(count, item_id)
-    item_endpoint = '/items/{}/metadata'.format(item_id)
-    full_link = baseURL+item_endpoint
-    r = requests.get(full_link, headers=header).json()
+    return a dict with a flat structure. where a field has multiple values,
+    these values are concatenated separated by a pipe character.
+    """
+    item_dict = {}
+    for fieldname, field_values in item["metadata"].items():
+        str_value = "|".join([i["value"] for i in field_values])
+        item_dict[fieldname] = str_value
+    return item_dict
 
 
-all_items = pd.DataFrame.from_dict(all_items)
-all_items.to_csv('item_metadata.csv', index=False)
+if __name__ == "__main__":
+    main()
