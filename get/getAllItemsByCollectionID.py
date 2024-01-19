e285992534bd41b9b53c988660efd33c75cba973
Noah Geraci <noah.geraci@jhu.edu>
2024-01-19 14:28:01 -0800

    first pass at rewriting getAllItemsByCollectionID to use the search endpoint

--- get/getAllItemsByCollectionID.py
+++ get/getAllItemsByCollectionID.py
@@ -1,51 +1,63 @@
+""" provide UUID for a DSpace 7 collection or community
+    output a CSV file with metadata for all items in that collection or community
+"""
+
 import requests
 import pandas as pd
 
-header = {'content-type': 'application/json'}
-baseURL = 'https://j10p-clone.library.jhu.edu'
-endpoint1 = '/server/api/core/collections/'
-endpoint2 = '/items'
-pagination = '?size=100'
+
+def main():
+    # it looks like the way to retrieve items by collection in DSpace 7 is via the search endpoint
+    # specify the collection or community UUID in the scope parameter
+    # see https://groups.google.com/g/dspace-tech/c/jZ5eSMl1c1U
+
+    # this example collection is the Baltimore City Sheet Maps collection
+    # expecting 427 items
+    collection_uuids = ["7e692697-d553-4887-8404-61a0821e42b8"]
+
+    base_url = "https://j10p-stage.library.jhu.edu"
+    timeout = 10
+    size = 100
+
+    all_items = []
+    for uuid in collection_uuids:
+        endpoint = f"{base_url}/server/api/discover/search/objects?scope={uuid}"
+        for page in get_paginated_data(endpoint, size, timeout):
+            results = page["_embedded"]["searchResult"]["_embedded"]["objects"]
+            for result in results:
+                item = result["_embedded"]["indexableObject"]
+                item_dict = json_to_flat_dict(item)
+                all_items.append(item_dict)
+
+    all_items = pd.DataFrame.from_dict(all_items)
+    all_items.to_csv("allCollectionItems.csv", index=False)
 
 
-collection_uuids = ['7e692697-d553-4887-8404-61a0821e42b8']
+def get_paginated_data(endpoint, size, timeout):
+    """generator function for paginated search results
+    uses the "next" links within the returned data
+    """
+    while endpoint:
+        r = requests.get(endpoint, timeout=timeout, params={"size": size})
+        if r.status_code == 200:
+            data = r.json()
+            yield data
+            next_link = data["_embedded"]["searchResult"]["_links"].get("next")
+            endpoint = next_link["href"] if next_link else None
 
-all_items = []
-for uuid in collection_uuids:
-    more_links = True
-    next_list = []
-    while more_links:
-        if not next_list:
-            full_link = baseURL+endpoint1+uuid+endpoint2+pagination
-            r = requests.get(full_link, headers=header).json()
-            results = r['page']
-            totalElements = results['totalElements']
-            print('Total elements: {}.'.format(totalElements))
-        else:
-            next_link = next_list[0]
-            r = requests.get(next_link).json()
-        communities = r['_embedded']['communities']
-        for community in communities:
-            comm_id = community['id']
-            uuid = community['uuid']
-            name = community['name']
-            handle = community['handle']
-            comm_dict = {'id': comm_id, 'uuid': uuid,
-                         'name': name, 'handle': handle}
-            all_items.append(comm_dict)
-        results = r['page']
-        totalPage = results['totalPages']
-        number = results['number']
-        number = number + 1
-        print('Page {} of {}.'.format(number, totalPage))
-        next_list.clear()
-        links = r.get('_links')
-        nextDict = links.get('next')
-        if nextDict:
-            next_link = nextDict.get('href')
-            next_list.append(next_link)
-        else:
-            break
 
-all_items = pd.DataFrame.from_dict(all_items)
-all_items.to_csv('allCommunityItems.csv', index=False)
+def json_to_flat_dict(item):
+    """take JSON item with nested/multi-value fields returned from DSpace 7 API
+
+    return a dict with a flat structure. where a field has multiple values,
+    these values are concatenated separated by a pipe character.
+    """
+    item_dict = {"uuid": item["uuid"]}
+    for fieldname, field_values in item["metadata"].items():
+        str_value = "|".join([i["value"] for i in field_values])
+        item_dict[fieldname] = str_value
+    return item_dict
+
+
+if __name__ == "__main__":
+    main()
