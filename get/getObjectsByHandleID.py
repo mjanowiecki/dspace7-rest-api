445e7e45b613bea70884d96f39672931e55fcc0a
Noah Geraci <noah.geraci@jhu.edu>
2024-01-22 11:18:15 -0800

    first pass at getting handle lookup working with api/pid/find endpoint

--- get/getObjectsByHandleID.py
+++ get/getObjectsByHandleID.py
@@ -1,30 +1,53 @@
-import requests
 import argparse
+import requests
 import pandas as pd
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-f', '--file')
-args = parser.parse_args()
 
-if args.file:
-    filename = args.file
-else:
-    filename = input('Enter filename (including \'.csv\'): ')
+def main():
+    # parse command line arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file")
+    args = parser.parse_args()
+    if args.file:
+        filename = args.file
+    else:
+        # prompt user to enter filename if not provided as arg
+        filename = input("Enter filename (including '.csv'): ")
 
-header = {'content-type': 'application/json'}
-baseURL = 'https://j10p-clone.library.jhu.edu'
+    # set base url, endpoint, and timeout
+    base_url = "https://j10p-stage.library.jhu.edu"
+    endpoint = "server/api/pid/find"
+    timeout = 10
 
-df = pd.read_csv(filename)
+    # iterate over input csv
+    df = pd.read_csv(filename)
+    all_items = []
+    for index, row in df.iterrows():
+        print(index, row["handle"])
+        # get object data
+        r = requests.get(f"{base_url}/{endpoint}", timeout=timeout, params={"id": row["handle"]})
+        if r.status_code == 200:
+            item_dict = json_to_flat_dict(r.json())
+            all_items.append(item_dict)
+        # TODO: error handling for timeouts and non-200 statuses
 
-all_items = []
-for count, row in df.iterrows():
-    row = row
-    h_prefix = row['handle_prefix']
-    h_suffix = row['handle_suffix']
-    handle_endpoint = '/items/{}/{}'.format(h_prefix, h_suffix)
-    full_link = baseURL+handle_endpoint
-    r = requests.get(full_link, headers=header).json()
+    all_items = pd.DataFrame.from_dict(all_items)
+    all_items.to_csv('handle_metadata.csv', index=False)
+
+def json_to_flat_dict(item):
+    """
+    take JSON item with nested/multi-value fields returned from DSpace 7 API
+
+    return a dict with a flat structure. where a field has multiple values,
+    these values are concatenated separated by a pipe character.
+    """
+    item_dict = {"uuid": item["uuid"]}
+    for fieldname, field_values in item["metadata"].items():
+        str_value = "|".join([i["value"] for i in field_values])
+        item_dict[fieldname] = str_value
+    return item_dict
 
 
-all_items = pd.DataFrame.from_dict(all_items)
-all_items.to_csv('handle_metadata.csv', index=False)
+
+if __name__ == "__main__":
+    main()
