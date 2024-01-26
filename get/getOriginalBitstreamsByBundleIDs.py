3ab6bf77dd5c4b77a6af64198d15d7e8576e791c
Michelle Janowiecki <32551917+mjanowiecki@users.noreply.github.com>
2024-01-26 09:43:06 -0500

    updates

--- get/getOriginalBitstreamsByBundleIDs.py
+++ get/getOriginalBitstreamsByBundleIDs.py
@@ -1,48 +1,37 @@
-import requests
 import pandas as pd
+from get_utils import get_paginated_data
+from datetime import datetime
+
+scriptStart = datetime.now()
+
+df = pd.read_csv('test.csv')
 
 
-headers = {'content-type': 'application/json'}
-baseURL = 'https://jscholarship.library.jhu.edu'
+def main():
 
-df = pd.read_csv('originalBundlesByItemIDs.csv')
+    base_url = 'https://jscholarship.library.jhu.edu'
+    timeout = 10
+    size = 100
 
-all_items = []
-for count, row in df.iterrows():
-    row = row
-    bundle_uuid = row['bundle_uuid']
-    print(count, bundle_uuid)
-    more_links = True
-    next_list = []
-    while more_links:
-        if not next_list:
-            full_bitstream_link = baseURL+'/server/api/core/bundles/'+bundle_uuid+'/bitstreams'
-            print(full_bitstream_link)
-            try:
-                r = requests.get(full_bitstream_link, headers=headers, timeout=5).json()
-            except requests.exceptions.Timeout:
-                print('error!')
-                continue
-        else:
-            next_link = next_list[0]
-            print(next_link)
-            r = requests.get(next_link, headers=headers, timeout=5).json()
-        bitstreams = r['_embedded']['bitstreams']
-        for bitstream in bitstreams:
-            bitstream_uuid = bitstream['uuid']
-            bitstream_name = bitstream['name']
-            row['bitstream_uuid'] = bitstream_uuid
-            row['bitstream_name'] = bitstream_name
-            all_items.append(row)
-        next_list.clear()
-        search_results = r['_links']
-        next_page = search_results.get('next')
-        if next_page:
-            next_link = next_page.get('href')
-            next_list.append(next_link)
-        else:
-            break
+    all_items = []
+    for count, row in df.iterrows():
+        bundle_uuid = row['bundle_uuid']
+        endpoint = f"{base_url}/server/api/core/bundles/{bundle_uuid}/bitstreams"
+        for page in get_paginated_data(endpoint, size, timeout):
+            bitstreams = page['_embedded']['bitstreams']
+            for bitstream in bitstreams:
+                log = row.copy()
+                bitstream_uuid = bitstream['uuid']
+                bitstream_name = bitstream['name']
+                log['bitstream_uuid'] = bitstream_uuid
+                log['bitstream_name'] = bitstream_name
+                all_items.append(log)
+
+    all_items = pd.DataFrame.from_records(all_items)
+    all_items.to_csv('originalBitstreamsByBundleIDs.csv', index=False)
 
 
-all_items = pd.DataFrame.from_dict(all_items)
-all_items.to_csv('originalBitstreamsByBundleIDs.csv', index=False)
+if __name__ == "__main__":
+    main()
+
+print(datetime.now() - scriptStart)
--- get/getOriginalBundlesByItemIDs.py
+++ get/getOriginalBundlesByItemIDs.py
@@ -1,45 +1,39 @@
-import requests
 import pandas as pd
+from get_utils import get_paginated_data
+from datetime import datetime
 
-headers = {'content-type': 'application/json'}
-baseURL = 'https://jscholarship.library.jhu.edu'
+scriptStart = datetime.now()
+
+df = pd.read_csv('test.csv')
 
 
-df = pd.read_csv('testing.csv')
+def main():
 
-all_items = []
-for count, row in df.iterrows():
-    row = row
-    item_uuid = row['item_uuid']
-    print(count, item_uuid)
-    more_links = True
-    next_list = []
-    while more_links:
-        if not next_list:
-            full_bundle_link = baseURL+'/server/api/core/items/'+item_uuid+'/bundles'
-            r = requests.get(full_bundle_link, headers=headers, timeout=5).json()
-        else:
-            next_link = next_list[0]
-            r = requests.get(next_link, headers=headers, timeout=5).json()
-        bundles = r['_embedded']['bundles']
-        for bundle_count, bundle in enumerate(bundles):
-            bundle_uuid = bundle['uuid']
-            bundle_name = bundle['name']
-            if bundle_name == 'ORIGINAL':
-                bundle_uuid = bundle['uuid']
-                row['bundle_name'] = bundle_name
-                row['bundle_uuid'] = bundle_uuid
-                all_items.append(row)
-            else:
-                pass
-        next_list.clear()
-        search_results = r['_links']
-        next_page = search_results.get('next')
-        if next_page:
-            next_link = next_page.get('href')
-            next_list.append(next_link)
-        else:
-            break
+    base_url = 'https://jscholarship.library.jhu.edu'
+    timeout = 10
+    size = 100
 
-all_items = pd.DataFrame.from_dict(all_items)
-all_items.to_csv('originalBundlesByItemIDs.csv', index=False)
+    all_items = []
+    for count, row in df.iterrows():
+        item_uuid = row['uuid']
+        endpoint = f"{base_url}/server/api/core/items/{item_uuid}/bundles"
+        print(count, item_uuid)
+        for page in get_paginated_data(endpoint, size, timeout):
+            bundles = page['_embedded']['bundles']
+            for bundle in bundles:
+                log = row.copy()
+                bundle_name = bundle['name']
+                if bundle_name == 'ORIGINAL':
+                    bundle_uuid = bundle['uuid']
+                    log['bundle_name'] = bundle_name
+                    log['bundle_uuid'] = bundle_uuid
+                    all_items.append(log)
+
+    all_items = pd.DataFrame.from_records(all_items)
+    all_items.to_csv('originalBundlesByItemIDs.csv', index=False)
+
+
+if __name__ == "__main__":
+    main()
+
+print(datetime.now() - scriptStart)
